f1a935c201b86dcbeb955a279523addd9920016e
Vijay Tadinada <pok8@cdc.gov>
2023-09-27 10:44:07 -0700

    Pushed from Domino: https://cdc-sandbox.domino-eval.com/workspace/pok8/Heart-Attack-prediction?executionId=650de0de5ab24c14cb26acb6

---
+++ heart-attack-predict.py
@@ -0,0 +1,36 @@
+import pickle
+import uuid
+import datetime
+import numpy as np
+
+
+import mlflow.pyfunc
+
+model = mlflow.pyfunc.load_model(model_uri=f"models:/heart-attack-LR-model/1")
+
+# from domino_prediction_logging.prediction_client import PredictionClient
+from domino_data_capture.data_capture_client import DataCaptureClient
+
+features = ['thal','exang','cp','ca','sex','oldpeak','slope']
+
+target = ["target"]
+
+# pred_client = PredictionClient(features, target)
+data_capture_client = DataCaptureClient(features, target)
+
+def predict(thal,exang,cp,ca,sex,oldpeak,slope, _id=None):
+    feature_values = [density, volatile_acidity, chlorides, is_red, alcohol]
+    prediction = model.predict([feature_values]).tolist()
+
+
+    # Record eventID and current time
+    if _id is None:
+        print("No ID found! Creating a new one.")
+        wine_id = str(datetime.datetime.now())
+        # custid = uuid.uuid4()
+    print('ID is: {}'.format(_id))
+
+    # pred_client.record(feature_values, prediction, event_id=custid)
+    data_capture_client.capturePrediction(feature_values, prediction,event_id=_id)
+
+    return dict(prediction=prediction[0])
