c766fedf578580b1d3cf324cb1edb184711ec2ff
Vijay Tadinada <pok8@cdc.gov>
2023-10-23 14:45:44 -0700

    Pushed from Domino: https://cdc-sandbox.domino-eval.com/workspace/pok8/Heart-Attack-prediction?executionId=650de0de5ab24c14cb26acb6

--- app.sh
+++ app.sh
@@ -5,9 +5,9 @@
 # quick-start project.
  
 # # Python/Flask example
-# export LC_ALL=C.UTF-8
-# export LANG=C.UTF-8
-# export FLASK_APP=app-flask.py
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+export FLASK_APP=app-flask.py
 pip install -r requirements_apps.txt --user
 python -m flask run --host=0.0.0.0 --port=8888
  
